e2b1703aeabd0614a7e165e79f0e03ba9bc6da7d
Jensen Kershey Aragon <jnsnaragon01@gmail.com>
2025-01-06 05:22:49 +0800

    add analyics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google';
 import './globals.css';
 import Header from './components/Header';
 import Link from 'next/link';
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -21,6 +22,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Header />
         {children}
+        <Analytics />
         <footer className="bg-zinc-950 text-white pt-20 pb-10">
           <div className="container mx-auto px-4">
             {/* Main Footer Content */}
